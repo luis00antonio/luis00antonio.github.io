773274a558082a4fe1d3f25c905988a63fc22b31
Luis Antonio López, LAL <130500616+luis00antonio@users.noreply.github.com>
2023-05-10 18:51:47 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+const myImage = document.querySelector("img");
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox.png");
+  }
+};
+
+function setUserName() {
+  const myName = prompt("Please enter your name.");
+  if (!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `Mozilla is cool, ${myName}`;
+  }
+}
+
+
+myButton.onclick = () => {
+  setUserName();
+};
+
+
